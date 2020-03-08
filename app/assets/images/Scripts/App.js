6ea0969b4d0c682e3aff220626ae64932a4a771a
mr-blankson <fat_tonyy@icloud.com>
2020-03-08 14:58:52 +0000

    worked-on-mobile-menu

--- app/assets/images/Scripts/App.js
+++ app/assets/images/Scripts/App.js
@@ -1,5 +1,11 @@
+//?below is for css configuration using webpack
 import "../../styles/styles.scss";
 
+//? below is to make webpack read and refreshes if any changes is made
 if (module.hot) {
   module.hot.accept();
 }
+
+//? module imports from module folder
+import MobileMenu from "./modules/MobileMenu";
+let mobilemenu = new MobileMenu();
