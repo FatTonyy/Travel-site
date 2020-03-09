d2128e04054f853b2033f6b7e347e1bb2ce0ab67
mr-blankson <fat_tonyy@icloud.com>
2020-03-09 18:13:25 +0000

    completed-sticky-header

--- app/assets/images/Scripts/App.js
+++ app/assets/images/Scripts/App.js
@@ -9,8 +9,14 @@ if (module.hot) {
 //? module imports from module folder
 import MobileMenu from "./modules/MobileMenu";
 import Reveal from "./modules/RevealOnScroll";
+import StickyHeader from "./modules/sticky-header";
 
+// creating a new variable for sticky header
+let stickyHeader = new StickyHeader();
+
+// for reveal-scroll-animation
 new Reveal(document.querySelectorAll(".feature-item"), 75);
 new Reveal(document.querySelectorAll(".testimonial"), 60);
 
+// for hamburger-meu
 let mobilemenu = new MobileMenu();
