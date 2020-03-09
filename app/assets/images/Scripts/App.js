6ea2d0098a1eade2c54fd0ace46414900d668707
mr-blankson <fat_tonyy@icloud.com>
2020-03-09 21:17:47 +0000

    updated-for-modal-section

--- app/assets/images/Scripts/App.js
+++ app/assets/images/Scripts/App.js
@@ -20,3 +20,22 @@ new Reveal(document.querySelectorAll(".testimonial"), 60);
 
 // for hamburger-meu
 let mobilemenu = new MobileMenu();
+
+// modal [below is a piece of javascript to help client save data if they are not going to tab on the button]
+let modal;
+
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault();
+    if (typeof modal === "undefined") {
+      import(/* webpackChunkName: 'modal' */ "./modules/Modal")
+        .then(x => {
+          modal = new x.default();
+          setTimeout(() => modal.openTheModal(), 20);
+        })
+        .catch(() => console.log("there was a problem"));
+    } else {
+      modal.openTheModal();
+    }
+  });
+});
